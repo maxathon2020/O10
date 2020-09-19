5c465152553972a7a3d1887e9c519640bdaa98ad
Kirill Gandyl <muaddibco@gmail.com>
2020-09-19 04:33:59 +0300

    biometry related controllers added

---
+++ O10.Web.Server/O10.Web.Server/Configuration/IPortalConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using O10.Core.Configuration;
+
+namespace O10.Web.Server.Configuration
+{
+    public interface IPortalConfiguration : IConfigurationSection
+    {
+        string FacePersonGroupId { get; set; }
+        bool DemoMode { get; set; }
+    }
+}
